fd2ee6aec5b7169a2ce7e8d78380673229e64aa0
TedoDonchev <37153281+TedoDonchev@users.noreply.github.com>
2023-11-14 15:44:05 +0200

    initial commit

---
+++ 04.Associative Arrays/src/com/company/_01_CountCharsInAString.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class _01_CountCharsInAString {
+
+    public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+
+	    String[] input = sc.nextLine().split(" ");
+
+        System.out.println("azis");
+
+    }
+}
